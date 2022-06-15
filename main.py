3c4ac0bf3b1b70a91eccd7892a5b027050f044cc
Francisco Andrade <francisco@inspr.com>
2022-06-14 22:06:07 -0300

    Add CI/CD initial config

---
+++ main.py
@@ -0,0 +1,10 @@
+# Req example
+# curl -X GET -H "Authorization: Bearer <BEARER TOKEN>" "https://api.twitter.com/2/tweets/440322224407314432"
+
+# Resp example
+# {
+#   "data": {
+#     "id": "440322224407314432",
+#     "text": "If only Bradley's arm was longer. Best photo ever. #oscars http://t.co/C9U5NOtGap"
+#   }
+# }
