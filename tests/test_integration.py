94a6a74e468fb2a49da6b4c9d2787865c5596ad4
Francisco Andrade <francisco@inspr.com>
2022-06-17 17:33:53 -0300

    Trying to make integration tests work

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -4,10 +4,13 @@ import pytest
 from modules.api import create_user_request_url, send_request
 from modules.ttprocess import get_hashtags
 
-bearer_token = os.environ.get("TT_BEARER")
+
+@pytest.fixture
+def bearer():
+    return os.environ.get("TT_BEARER")
 
 
-def test_valid_request():
+def test_valid_request(bearer):
     url = create_user_request_url("TwitterDev")
-    resp = send_request(url, bearer_token)
+    resp = send_request(url, bearer)
     assert resp["data"]["username"] == "TwitterDev"
